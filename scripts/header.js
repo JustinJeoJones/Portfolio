d44799133cb31e3e7d9104993261806702101c1b
Jeo <jeo961@gmail.com>
2020-07-29 15:12:25 -0400

    first commit

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+const header = <div className="Header">
+            <a href="index.html">Justin Jones</a>
+            <a href="projects.html">Projects</a>
+            <a href="contact.html">Contact Me!</a>
+        </div>;
+
+ReactDOM.render(
+    header,
+    document.getElementById('header')
+  );
