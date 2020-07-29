a0616cf909e218adfe6967e7aa58c4943b2138d4
Jeo <jeo961@gmail.com>
2020-07-29 18:04:10 -0400

    added babel and react

--- scripts/header.js
+++ scripts/header.js
@@ -1,10 +1,10 @@
-const header = <div className="Header">
-            <a href="index.html">Justin Jones</a>
-            <a href="projects.html">Projects</a>
-            <a href="contact.html">Contact Me!</a>
-        </div>;
-
+//Placed near the top of page. 
+const header =  <div className="Header">
+            <a href="Index.html">Justin Jones</a>
+            <a href="Projects.html"></a>
+            <a href="Contact.html">Contact Me!</a>
+        </div>
 ReactDOM.render(
     header,
     document.getElementById('header')
-  );
+);
