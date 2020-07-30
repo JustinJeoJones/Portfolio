63a8570f84b9ed36e29bd11189437683d928200a
Jeo <jeo961@gmail.com>
2020-07-29 21:30:22 -0400

    all thats left is the pdf

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,7 +3,7 @@ const contact =
     <h1 className = "holderTitle">Contact me!</h1>
     <div className="contact">
         <p className="text">If you wish to contact me, please email me at Jeo961@gmail.com and I will get back to you in a couple business days</p>
-        <p className="text"><a href="./files/Justin_Jones_Resume.PDF" download>Click here</a> to download my Resume!</p>
+        <p className="text"><a href="files/Justin_Jones_Resume.pdf" download>Click here</a> to download my Resume!</p>
     </div>
 </div>;
 
