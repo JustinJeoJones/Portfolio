85100b9c17e4e7eafb62bef67ac7b34b9b1c3337
Jeo <jeo961@gmail.com>
2020-07-29 21:20:46 -0400

    video works, image broke

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,7 @@
 //Placed near the top of page. 
 const header =  
 <div className="header">
-    <img className="profile" src="./images/profile.JPG"></img>
+    <img className="profile" src="images/profile.JPG"></img>
     <a href="index.html">Justin Jones</a>
 </div>
 ReactDOM.render(
